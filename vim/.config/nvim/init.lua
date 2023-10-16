d1749760df4249ea9a1b0144fb2d80318c0f659b
Calin Martinconi <martinconic@gmail.com>
2023-10-16 21:07:33 +0300

    small change

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -170,7 +170,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'nightfox',
         component_separators = '|',
         section_separators = '',
       },
